14b9300eea948b6f922181efb2497839c5d6ad46
AprupeSistema <aprupesistema@example.com>
2026-09-12 20:33:21 +0300

    Update GAS_URL to latest deployment

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   APP_NAME: 'Aprūpes sistēma',
   VERSION: '1.0.0',
 
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbzqubxNyVRHgcrvkZkn_EVJIaAqhrOPHQoAmdkDurNQT12vsDuYuPuUxwlC9durOmus6g/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbzK18jMF9CDDwCAYhAybrz6-yypd0LjmEHOKTCIkrv8bqp00NI2PU32mvuRdZDF7PIEqA/exec',
 
   SHEET_ID: '1OQAdiHsuQEwy180b68oHQ9xxELFV2_CkqDJY7ej0P5E',
 
