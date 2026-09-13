b0d9db18ad0ed8f9bca6fb48827c6d734b61bd98
AprupeSistema <aprupesistema@example.com>
2026-09-13 12:00:58 +0300

    mainīta atslēga

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   APP_NAME: 'Aprūpes sistēma',
   VERSION: '1.0.0',
 
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbwXWV9adYC--3HreeZP0ZHMxGyjofy28zJgwwrve3_VVLIbWnM0jhlvgHzcWsEjielWEw/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbxHijHCVL75IyMvvff_En3W_RnNF7qBMNygL0zVV_Z9y_xbrL6D-9Uoi9GrSSVW5a2xYw/exec',
 
   SHEET_ID: '1OQAdiHsuQEwy180b68oHQ9xxELFV2_CkqDJY7ej0P5E',
 
