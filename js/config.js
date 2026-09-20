caba5b67c0645e7a32330f5ea7d253e70b00a1eb
AprupeSistema <aprupesistema@example.com>
2026-09-21 00:03:02 +0300

    config: jauns GAS_URL (AKfycbz3seUbi...) + cache-bust 20260920-2010

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   APP_NAME: 'Aprūpes sistēma',
    VERSION: '1.0.1',
 
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbyjJ-PCRPDlKuJ-5J6oh8nXsujJ27U6BPWp5Wdz_qZjMJDzAjQlr1NKnNZFnnQx4wEMuA/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbz3seUbiPFWieeju26EunPx7HLhC6_t1DmNt4La3dpi_XUQ2dS0dy8tjK6ARYtBbRxqgw/exec',
 
   SHEET_ID: '1OQAdiHsuQEwy180b68oHQ9xxELFV2_CkqDJY7ej0P5E',
 
