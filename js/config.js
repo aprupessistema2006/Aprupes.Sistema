3e6aabc11b67dc6882884fd5da1e6c35cb2bf16d
AprupeSistema <aprupesistema@example.com>
2026-09-22 22:40:26 +0300

    Update GAS_URL to new deployment after handleMark fix

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   APP_NAME: 'Aprūpes sistēma',
   VERSION: '1.0.2',
 
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbwDjkpvz4DHxK8Qi6dRTm3LI7iPtTP85XthWAqsDPKtvrEkKGn85QHLKeRd06hq7Vw7vw/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbzrV0H7033UKr_ItwRafazrCEVXNN8zKdcrV154y-8sR8W7y3chADswgvUgqLndsDhifg/exec',
 
   SHEET_ID: '1OQAdiHsuQEwy180b68oHQ9xxELFV2_CkqDJY7ej0P5E',
 
