7993607e034213fb9a217a973a5260dcf3d4bb42
AprupeSistema <aprupesistema@example.com>
2026-09-23 23:17:03 +0300

    v20260923-1900: Fix update_notifier.js versionParam (v1855->v1900) to ensure SW re-registration and cache-busting works for the new GAS_URL deployment

--- js/update_notifier.js
+++ js/update_notifier.js
@@ -5,7 +5,7 @@
  */
 class UpdateNotifier {
   constructor() {
-    this.versionParam = '?v=20260923-1855';
+    this.versionParam = '?v=20260923-1900';
     this.init();
     this.checkVersionOnPageLoad();
   }
