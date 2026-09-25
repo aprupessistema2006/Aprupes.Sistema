e4d81129e5b45ca3afe5fb5f7e0c7dce4802623f
AprupeSistema <aprupesistema@example.com>
2026-09-25 16:16:18 +0300

    v20260925-1610: bump versija + SW v18
    Nodrošina, ka klients saņem jauno sync.js ar retry/marks-paging kodu.
    SW v18 tīra veco v17 kešu, piespieduši ielādējot visus aģentus no tīkla.

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   APP_NAME: 'Aprūpes sistēma',
-  VERSION: '20260925-1604',
+  VERSION: '20260925-1610',
 
   GAS_URL: 'https://script.google.com/macros/s/AKfycby_iNzXWP-CABf8ZxCv5Zd2yyXZ3fW7OQmQtRQB6_SbQNz3DGtj4yBmnw2iTP3KzUB8iw/exec',
 
